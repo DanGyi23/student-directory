29e8243a63b35181876e1f815f0d91b957e8b144
Dan Gyi <daniel.gyi@gmail.com>
2019-08-04 15:45:43 +0100

    remove hardcoded names, add name input

--- directory.rb
+++ directory.rb
@@ -1,19 +1,3 @@
-# places all the students in an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :November},
-  {name: "Darth Vader", cohort: :November},
-  {name: "Nurse Ratched", cohort: :November},
-  {name: "Michael Corleone", cohort: :November},
-  {name: "Alex DeLarge", cohort: :November},
-  {name: "The Wicked Witch of the West", cohort: :November},
-  {name: "Terminator", cohort: :November},
-  {name: "Freddy Krueger", cohort: :November},
-  {name: "The Joker", cohort: :November},
-  {name: "Joffrey Baratheon", cohort: :November},
-  {name: "Norman Bates", cohort: :November}
-]
-
-# wrap everything in methods
 def print_header
   puts "The students of Villains Academy\n-------------"
 end
@@ -26,7 +10,25 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-# call the methods with students as param for print_students and print_footer
+# name injector - asks user for names of students, adds them to the array, then
+# puts a running count.
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+  name = gets.chomp
+
+  while !name.empty? do
+    students << {name: name, cohort: :November}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  #returns complete array with all added names
+  students
+end
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
