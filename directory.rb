1bbecf6fdf2542a1fbbd78b57b902e2b0fe13b07
Dan Gyi <daniel.gyi@gmail.com>
2019-08-04 14:59:23 +0100

    create and call methods for printing

--- directory.rb
+++ directory.rb
@@ -2,21 +2,33 @@ puts "The students of Villains Academy\n-------------"
 
 # places all the students in an array
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon",
-"Norman Bates"
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
 ]
 
-#prints students, one per line
-students.each { |student| puts student }
+# wrap everything in methods
+def print_header
+  puts "The students of Villains Academy\n-------------"
+end
 
-# finally we print the count of the students, using print to avoid newline
-puts "Overall, we have #{students.count} great students"
+def print_students(names)
+  names.each { |student| puts student }
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+# call the methods with students as param for print_students and print_footer
+print_header
+print_students(students)
+print_footer(students)
