f96981c163f2022ed2c0121bce22f7a4764d2c41
Dan Gyi <daniel.gyi@gmail.com>
2019-08-05 17:19:28 +0100

    Exercise 8, add grouped by cohort

--- directory.rb
+++ directory.rb
@@ -1,4 +1,5 @@
 @max_size = []
+@students = []
 
 def print_header
   puts "The students of Villains Academy".center(max_len)
@@ -6,8 +7,8 @@ def print_header
 end
 
 def print(students)
-  students.each.with_index(1) do |student,index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort) // DOB #{student[:dob]} // Hobbies: #{student[:hobbies]}".center(max_len)
+  @students.each.with_index(1) do |student,index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(max_len)
   end
 end
 
@@ -19,45 +20,40 @@ def print_footer(names)
   end
 end
 
-# added in hobbies and dob inputs
-def input_students
-  students = []
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return three times"
+  def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return three times"
 
-  name = gets.chomp
-  puts "Please enter this student's hobbies"
-  hobbies = gets.chomp
-  puts "Please enter this student's date of birth (dd/mm/yyyy)"
-  dob = gets.chomp
-  puts "Please enter which cohort you're part of"
-  cohort = gets.chomp
-
-  while !name.empty? do
-    students << {name: name, cohort: cohort, hobbies: hobbies, dob: dob}
-    @max_size << (name + hobbies + dob)
-    if students.size == 1
-      puts "Now we have #{students.count} student"
-    else
-      puts "Now we have #{students.count} students"
-    end
     name = gets.chomp
-    puts "Please enter this student's hobbies"
-    hobbies = gets.chomp
-    puts "Please enter this student's date of birth (dd/mm/yyyy)"
-    dob = gets.chomp
-    puts "Please enter which cohort you're part of"
+    puts "Please enter which month this student started"
     cohort = gets.chomp
+
+    while !name.empty? do
+      @students << {name: name, cohort: cohort}
+      @max_size << (name + cohort)
+      if @students.size == 1
+        puts "Now we have #{@students.count} student"
+      else
+        puts "Now we have #{@students.count} students"
+      end
+      name = gets.chomp
+      puts "Please enter which month this student started"
+      cohort = gets.chomp
+    end
+    @max_size
+    @students
   end
-  @max_size
-  students
-end
 
 def max_len
   @max_size.max_by(&:length).length + 33
 end
 
+def group_by_cohort(students)
+  @students.sort! { |a,b| b[:cohort] <=> a[:cohort] }
+end
+
 students = input_students
+group_by_cohort(students)
 print_header
 print(students)
 print_footer(students)
