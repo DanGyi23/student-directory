482ffdb47e97f7fa874ffcd0d150cb72dd9fa23a
Dan Gyi <daniel.gyi@gmail.com>
2019-08-05 08:57:33 +0100

    refactor interactive menu

--- directory.rb
+++ directory.rb
@@ -1,9 +1,12 @@
+#  make students accessible in all methods
+@students = []
+
 def print_header
   puts "The students of Villains Academy\n-------------"
 end
 
 def print(students)
-  students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
+  @students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" }
 end
 
 def print_footer(names)
@@ -16,40 +19,47 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
 
-  students = []
   name = gets.chomp
 
   while !name.empty? do
-    students << {name: name, cohort: :November}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :November}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  #returns complete array with all added names
-  students
+  @students
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    @students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+
 def interactive_menu
-  students = []
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-
-    selection = gets.chomp
-
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-    end
-
+    print_menu
+    # pass process as a method with user input as argument.
+    process(gets.chomp)
   end
 end
 
